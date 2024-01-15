656f45533a776e320bfb4f53c17ae7c717f01680
Sharit-Vargas07 <sharitvargas07@gmail.com>
2024-01-14 21:10:53 -0500

    avimel

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 60)
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+  
+    menu.addEventListener('click', function() {
+      menu.classList.toggle('bx-x');
+      navbar.classList.toggle('.open');
+    });
+  });
+  
+  
